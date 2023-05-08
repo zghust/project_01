6f8f7758d044bb4edcc007842915a9e83da413ab
zhanggang <zghust@163.com>
2023-05-08 09:39:57 +0800

    测试修改

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 import React from 'react'
 import Reactdom from 'react-dom'
-const a = React.createElement(
-  'div',
-  { class: 'wang', id: 'di' },
-  '我是一个盒子'
-)
-Reactdom.render(a, document.getElementById('root'))
+const abc = Reactdom.render(abc, document.getElementById('root'))
